d68a2d97adfc269c4e017e2bea80256cb6437faa
t3ch <w4d4f4k@gmail.com>
2023-08-09 14:36:09 +0100

    update

--- fwclear.sh
+++ fwclear.sh
@@ -3,9 +3,13 @@
 #
 iptables -F
 iptables -X
-# nat is used by wifihub
-iptables -t nat -F
-iptables -t nat -X
-#
-iptables -t mangle -F
-iptables -t mangle -X
+
+#--
+# (uncomment if you like to clean this too) -  nat is used by wifihub, docker etc...
+#iptables -t nat -F
+#iptables -t nat -X
+
+#--
+# (uncomment if you like to clean this too)
+#iptables -t mangle -F
+#iptables -t mangle -X
