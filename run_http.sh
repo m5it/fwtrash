d079b77ebc8ed5e5e3cf4b0b516fc2637a00e43a
t3ch <w4d4f4k@gmail.com>
2026-02-07 23:10:43 +0000

    updates v0.1

--- run_http.sh
+++ run_http.sh
@@ -11,4 +11,9 @@ if [[ $(cat $LOG | wc -l) == "0" ]]; then
 	exit
 fi
 #
-tail -f $LOG | ./fwtrash.py -D -P rules/http.rules -a allowedips.txt -o badips.out -O trash_http.out -p modules.http -s "date,ip,repeat,blocked,bruteforced,req;40,ref;10,ua;10,code,len" -S "[--DATE] - ([--REPEAT],[--CODE],[--LEN],[--BLOCKED],[--BRUTEFORCED]) [--IP] => [--REQ] ua: [--UA], ref: [--REF]" -c "iptables -A INPUT -s [--IP]/32 -j DROP" -b "key:1,climit:3,tlimit:5;key:2,climit:3,tlimit:6"
+tail -f $LOG | ./fwtrash.py -D \
+	-P rules/http.rules -a allowedips.txt -o badips.out -O trash_http.out \
+	-p modules.http -s "date,ip,repeat,blocked,bruteforced,req;40,ref;10,ua;10,code,len" \
+	-S "[--DATE] - ([--REPEAT],[--CODE],[--LEN],[--BLOCKED],[--BRUTEFORCED]) [--IP] => [--REQ] ua: [--UA], ref: [--REF]" \
+	-c "iptables -A INPUT -s [--IP]/32 -j DROP" \
+	-b "key:1,climit:3,tlimit:5;key:2,climit:3,tlimit:6;key:3,climit:1,tlimit:50;key:4,climit:1,tlimit:50"
