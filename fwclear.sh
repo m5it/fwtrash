20877c656c961aad92ce5ffc3795ccd71359f8a9
t3ch <w4d4f4k@gmail.com>
2026-02-10 13:05:19 +0000

    work on v0.6 and what is missing with fwspoof is here.. ** love you all my good friends

--- fwclear.sh
+++ fwclear.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+#--
+# Clear DROPed ips.
+#--
+# Usage: 
+#   sudo iptables -L INPUT -n | awk /DROP/'{print $4}' | sudo ./fwclear.sh
+#--
 while read -r L; do
 	echo "L: "$L
 	iptables -D INPUT -s $L -j DROP
